933121bb1a1447083258460c671451506108d58d
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-12 14:36:26 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -13,8 +13,8 @@ st.set_page_config(layout="wide")
 
 username=st.secrets["streamlit_username"]
 password=st.secrets["streamlit_password"]
-column_list = ['CASH_AND_CASH_EQUIVALENTS','SHORT_TERM_INVESTMENTS','RECEIVABLES','INVENTORY','OTHER_CURRENT_ASSETS','TOTAL_CURRENT_ASSETS','PROPERTY_PLANT_AND_EQUIPMENT','GOODWILL','INTANGIBLE_ASSETS','ACCUMULATED_AMORTIZATION','OTHER_ASSETS','TOTAL_ASSETS','ACCOUNTS_PAYABLE_AND_ACCRUED_EXPENSES','SHORT_OR_CURRENT_LONG_TERM_DEBT','OTHER_CURRENT_LIABILITIES','TOTAL_CURRENT_LIABILITIES','LONG_TERM_DEBT','OTHER_LIABILITIES','DEFERRED_LONG_TERM_LIABILITY_CHARGES','MINORITY_INTEREST','NEGATIVE_GOODWILL','TOTAL_LIABILITIES','MISC_STOCKS_OPTIONS_WARRANTS','REDEEMABLE_PREFERRED_STOCK','PREFERRED_STOCK','COMMON_STOCK','RETAINED_EARNINGS','TREASURY_STOCK','CAPITAL_SURPLUS','OTHER_STOCKHOLDER_EQUITY','TOTAL_STOCKHOLDER_EQUITY','NET_TANGIBLE_ASSETS','NET_INCOME','DEPRECIATION','CHANGES_IN_ACCOUNTS_RECEIVABLES','CHANGES_IN_ASSETS_AND_LIABILITIES','CHANGES_IN_INVENTORIES','CHANGES_IN_OTHER_OPERATING_ACTIVITIES','TOTAL_CASH_FLOW_FROM_OPERATING_ACTIVITIES','CAPITAL_EXPENDITURES','INVESTMENTS','OTHER_CASH_FLOWS_FROM_INVESTING_ACTIVITIES','TOTAL_CASH_FLOWS_FROM_INVESTING_ACTIVITIES','DIVIDENDS_PAID','SALE_OR_PURCHASE_OF_STOCK','NET_BORROWINGS','OTHER_CASH_FLOWS_FROM_FINANCING_ACTIVITIES','TOTAL_CASH_FLOWS_FROM_FINANCING_ACTIVITIES','CHANGE_IN_CASH_AND_CASH_EQUIVALENTS','TOTAL_REVENUE','COST_OF_REVENUE','GROSS_PROFIT','RESEARCH_DEVELOPMENT','SELLING_GENERAL_AND_ADMINISTRATIVE','NON_RECURRING','OTHERS','TOTAL_OPERATING_EXPENSES','OPERATING_INCOME_OR_LOSS','TOTAL_OTHER_INCOME_OR_EXPENSES_NET','EARNINGS_BEFORE_INTEREST_AND_TAXES','INTEREST_EXPENSE','INCOME_BEFORE_TAX','INCOME_TAX_EXPENSE','MINORITY_INTEREST','NET_INCOME_FROM_CONTINUING_OPS','DISCONTINUED_OPERATIONS','EXTRAORDINARY_ITEMS','EFFECT_OF_ACCOUNTING_CHANGES','OTHER_ITEMS','NET_INCOME','PREFERRED_STOCK_AND_OTHER_ADJUSTMENTS','NET_INCOME_APPLICABLE_TO_COMMON_SHARES']
-cutoff = 40
+column_list = ['CASH_AND_EQUIVALENTS','SHORT_TERM_INVESTMENTS','CASH_AND_SHORT_TERM_INVESTMENTS','NET_RECEIVABLES','INVENTORY','TOTAL_CURRENT_ASSETS','PROPERTY_PLANT_EQUIPMENTNET','GOODWILL','INTANGIBLE_ASSETS','GOOD_WILL_AND_INTANGIBLE_ASSETS','LONG_TERM_INVESTMENTS','TAX_ASSETS','OTHER_NON_CURRENT_ASSETS','TOTAL_NON_CURRENT_ASSETS','OTHER_ASSETS','TOTAL_ASSETS','ACCOUNTS_PAYABLES','SHORT_TERM_DEBT','TAX_PAYABLES','OTHER_CURRENT_LIABILITIES','TOTAL_CURRENT_LIABILITIES','LONG_TERM_DEBT','DEFERRED_REVENUE_NONCURRENT','DEFERRED_TAX_LIABILITIES_NONCURRENT','OTHER_NONCURRENT_LIABILITIES','TOTAL_LIABILITIES','RETAINED_EARNINGS','ACCUMULATED_OTHER_COMPREHENSIVE_INCOME','OTHER_TOTAL_STOCKHOLDERS_EQUIT','TOTAL_STOCKHOLDERS_EQUITY','TOTAL_EQUITY','TOTAL_LIABILITIES_AND_STOCKHOLDERS_EQUITY','MINORITY_INTEREST','TOTAL_LIABILITIES_AND_TOTAL_EQUITY','TOTAL_INVESTMENTS','TOTAL_DEBT','NET_DEBT','NET_INCOME','DEPRECIATION_AND_AMORTIZATION','DEFERRED_INCOME_TAX','STOCK_BASED_COMPENSATION','CHANGE_IN_WORKING_CAPITAL','ACCOUNTS_RECEIVABLES','INVENTORY','ACCOUNTS_PAYABLES','OTHER_WORKING_CAPITAL','OTHER_NON_CASH_ITEMS','NET_OPERATING_ACTIVITIES','INVESTMENTS_IN_PROPERTY_PLANT_AND_EQUIPMENT','ACQUISITIONS','PURCHASES_OF_INVESTMENTS','SALES_OF_INVESTMENTS','OTHER_INVESTING_ACTIVITES','NET_INVESTING_ACTIVITES','DEBT_PAYMENT','COMMON_STOCK_ISSUED','COMMON_STOCK_REPURCHASED','DIVIDENDS_PAID','OTHER_FINANCING_ACTIVITES','NET_FINANCING_ACTIVITIES','NET_CHANGE_IN_CASH','OPERATING_CASH_FLOW','CAPITAL_EXPENDITURE','FREE_CASH_FLOW','REVENUE','COST_OF_REVENUE','GROSS_PROFIT','GROSS_PROFIT_RATIO','RESEARCH_AND_DEVELOPMENT_EXPENSES','SELLING_GENERAL_AND_ADMINISTRATIVE_EXPENSES','OPERATING_EXPENSES','COST_AND_EXPENSES','INTEREST_INCOME','INTEREST_EXPENSE','DEPRECIATION_AND_AMORTIZATION','EBITDA_EARNINGS_BEFORE_INTEREST_TAX_DEPRECATION_AND_AMORITZATION','OPERATING_INCOME','OTHER_INCOME_EXPENSES','INCOME_BEFORE_TAX','INCOME_TAX_EXPENSE','NET_INCOME','EPS_EARNINGS_PER_SHARE','EPS_EARNINGS_PER_SHARE_DILUTED','WEIGHTED_AVERAGE_SHARES_OUTSTANDING','WEIGHTED_AVERAGE_SHARES_OUTSTANDING_DILUTED']
+cutoff = 20
 # establish snowpark connection
 conn = st.experimental_connection("snowpark")
 
@@ -30,21 +30,16 @@ st.cache_data(ttl=86400)
 def plot_financials(df_2, x, y, x_cutoff, title):
     """"
     helper to plot the altair financial charts
-   
+    
     return st.altair_chart(alt.Chart(df_2.head(x_cutoff)).mark_bar().encode(
         x=x,
         y=y
         ).properties(title=title)
     ) 
-  """ 
-    df_2 = pd.DataFrame(df_2)
-    #st.write("Function-",df_2)
+    """
     df_subset = df_2.head(x_cutoff)
-  
     # Create a bar chart using st.bar_chart()
-
-    return st.bar_chart(df_subset.set_index(x))
-
+    return st.bar_chart(df_subset.set_index(x)[y])
     
 def fs_chain(str_input):
     """
@@ -91,8 +86,8 @@ def authenticate_user():
 
 if authenticate_user():
     with st.sidebar:
-      image = Image.open("assets/FinGPT.png")
-      image = st.image('assets/FinGPT.png',width=280)
+      image = Image.open("streamlit-buffett-main/assets/FinGPT.png")
+      image = st.image('streamlit-buffett-main/assets/FinGPT.png',width=280)
       selected = option_menu( menu_title="Explore",
       menu_icon = "search",
       options=["Company Statements", 'Annual Reports'], 
@@ -109,7 +104,7 @@ if authenticate_user():
           **Some Sample Questions:**
       
           - What is the net income of Marvell in 2022?
-          - What is the gross profit in last 3 years?
+          - What is the gross profit of Marvell in last 3 years?
         
         
         """)
@@ -126,20 +121,16 @@ if authenticate_user():
                     continue
                 csv = StringIO(df_str)
                 df_data = pd.read_csv(csv, sep=',')
-                #st.write("Function2-",df_data)
                 col1, col2 = st.columns(2)
                 df_data.columns = df_data.columns.str.replace('_', ' ')
                 headers = df_data.columns
                 
                 with col1:
-                    st.markdown(tabulate(df_data, tablefmt="html",headers=headers,showindex=False), unsafe_allow_html = True)
-                    #st.write(df_str)
-                    
+                    st.markdown(tabulate(df_data, tablefmt="html",headers=headers,showindex=False), unsafe_allow_html = True) 
                     if len(df_data.index) >2 & len(df_data.columns) == 2:
-                        y = list(df_data.columns[1:])
                         title_name = df_data.columns[0]+'-'+df_data.columns[1]
                         with col2:
-                            plot_financials(df_data,df_data.columns[0],y, cutoff,title_name)
+                            plot_financials(df_data,df_data.columns[0],df_data.columns[1], cutoff,title_name)
         
         if prompt := str_input:
             st.chat_message("user").markdown(prompt, unsafe_allow_html = True)
@@ -148,7 +139,6 @@ if authenticate_user():
 
             try:
                 output = fs_chain(str_input)
-                #st.write(output['result'])
                 try:
                     # if the output doesn't work we will try one additional attempt to fix it
                     query_result = sf_query(output['result'])
@@ -166,29 +156,23 @@ if authenticate_user():
                         headers = df_2.columns
                         with col1:
                          st.markdown(tabulate(df_2, tablefmt="html",headers=headers,showindex=False), unsafe_allow_html = True) 
-                         
                         if len(df_2.index) >2 & len(df_2.columns) == 2:
-                            y = list(df_2.columns[1:])
                             title_name = df_2.columns[0]+'-'+df_2.columns[1]
                             with col2:
-                                plot_financials(df_2,df_2.columns[0],y, cutoff,title_name)
-                             #st.write(df_2)
+                             plot_financials(df_2,df_2.columns[0],df_2.columns[1], cutoff,title_name)
                       #st.session_state.messages.append({"role": "assistant", "content": tabulate(df_2, tablefmt="html",headers=headers,showindex=False)})
                         st.session_state.messages.append({"role": "assistant", "content": df_2.to_csv(sep=',', index=False)})
-                        
                     else:
                       with st.chat_message("assistant"):
                         st.write("Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports .")
       
-                except:                   
-                      #st.session_state.messages.append({"role": "assistant", "content": "The first attempt didn't pull what you were needing. Trying again..."})
-                      output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
-                      st.write(sf_query(output['result']))
+                except: 
+                    st.session_state.messages.append({"role": "assistant", "content": "The first attempt didn't pull what you were needing. Trying again..."})
+                    output = fs_chain(f'You need to fix the code but ONLY produce SQL code output. If the question is complex, consider using one or more CTE. Examine the DDL statements and answer this question: {output}')
+                    st.write(sf_query(output['result']))
             except:
-              with st.chat_message("assistant"):
-                st.markdown("Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports .")
-                #st.session_state.messages.append({"role": "assistant", "content": "Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports ."})
-              
+                st.session_state.messages.append({"role": "assistant", "content": "Please try to improve your question. Note this tab is for financial statement questions. Use Tab 2 to ask from Annual Reports ."})
+                #sst.write(output)
     else:
         query = st.chat_input("Enter your question:")
         st.markdown("""
