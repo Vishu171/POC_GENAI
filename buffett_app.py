1749d4cd4bb82b2043a1a49fa5cff02f09f24b8b
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-13 11:42:45 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -137,7 +137,7 @@ if authenticate_user():
                     if len(df_data.index) >2 & len(df_data.columns) == 2:
                         title_name = df_data.columns[0]+'-'+df_data.columns[1]
                         with col2:
-                            plot_financials(df_data,df_data.columns[0],df_data.columns[1], cutoff,title_name)
+                            plot_financials(df_data,df_data.columns[0],df_data.columns[-1], cutoff,title_name)
         
         if prompt := str_input:
             st.chat_message("user").markdown(prompt, unsafe_allow_html = True)
@@ -166,7 +166,7 @@ if authenticate_user():
                         if len(df_2.index) >2 & len(df_2.columns) == 2:
                             title_name = df_2.columns[0]+'-'+df_2.columns[1]
                             with col2:                                  
-                              plot_financials(df_2,df_2.columns[0],df_2.columns[1], cutoff,title_name)
+                              plot_financials(df_2,df_2.columns[0],df_2.columns[-1], cutoff,title_name)
                       #st.session_state.messages.append({"role": "assistant", "content": tabulate(df_2, tablefmt="html",headers=headers,showindex=False)})
                         st.session_state.messages.append({"role": "assistant", "content": df_2.to_csv(sep=',', index=False)})
                     else:
