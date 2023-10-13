7a8e2f24f8867324d65a9a6757212777de63d680
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-13 11:11:12 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -39,7 +39,7 @@ def plot_financials(df_2, x, y, x_cutoff, title):
     """
     df_subset = df_2.head(x_cutoff)
     # Create a bar chart using st.bar_chart()
-    return st.bar_chart(df_subset.set_index(x)[y],width=0, height=0, use_container_width=True)
+    return st.bar_chart(data=df_subset.set_index(x)[y],*,x=None, y=None, color=None,width=0, height=5, use_container_width=True)
 
 def kpi_recent(df_2, metric, periods=2, unit=1000000000):
     """
