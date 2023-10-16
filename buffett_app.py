aa50ac8357bb7187798bb9f697b3a7ca4fbd8c4d
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-16 20:56:05 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -38,9 +38,10 @@ def plot_financials(df_2, x, y, x_cutoff, title):
         ).properties(title=title)
     ) 
     """
-    df_subset = df_2.head(x_cutoff)
+    #df_subset = df_2.head(x_cutoff)
+    df = pd.DataFrame(df_2)
     # Create a bar chart using st.bar_chart()
-    return st.bar_chart(data=df_subset.set_index(x),x=None, y=None, color=None,width=0, height=300, use_container_width=True)
+    return st.bar_chart(data=df,x=df.columns[0], y=df.columns[1:], color=None,width=0, height=300, use_container_width=True)
     
 def fs_chain(str_input):
     """
