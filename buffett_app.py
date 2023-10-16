6ade00bab94760537b1dc6e864d55464ecd72df5
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-16 21:19:17 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -28,6 +28,13 @@ except:
 # adding this to test out caching
 st.cache_data(ttl=86400)
 
+def convert_to_numeric(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
+        
+
 def plot_financials(df_2, x, y, x_cutoff, title):
     """"
     helper to plot the altair financial charts
@@ -40,6 +47,7 @@ def plot_financials(df_2, x, y, x_cutoff, title):
     """
     #df_subset = df_2.head(x_cutoff)
     df = pd.DataFrame(df_2)
+    df = df.applymap(convert_to_numeric)
     # Create a bar chart using st.bar_chart()
     return st.bar_chart(data=df,x=df.columns[0], y=df.columns[1:], color=None,width=0, height=300, use_container_width=True)
     
