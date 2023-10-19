7566d9cef369a5bbf5088f35300f8b02123e066f
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-19 15:28:47 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -77,7 +77,7 @@ if authenticate_user():
         
         """)
 
-        st.button(label = "Press the button for 2022 data", on_click = operation )
+        st.button(label = "Press the button to get the 2022 data", on_click = operation )
         
         # Create a text input to edit the selected question
         if "messages_1" not in st.session_state.keys():
