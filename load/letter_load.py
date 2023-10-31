5c7fd1f281e1de5c04020e01ea9202e28480144c
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-10-31 15:43:26 +0530

    Update letter_load.py

--- load/letter_load.py
+++ load/letter_load.py
@@ -49,11 +49,11 @@ def operation():
     )
     
     indexes = pinecone.list_indexes()  # checks if provided index exists
-    if index_name in indexes:
-        pinecone.delete_index(index_name)
+    #if index_name in indexes:
+        #pinecone.delete_index(index_name)
         #print("Deleted Index")
         ##st.write("Delete Index")
-        pinecone.create_index(index_name, dimension=1536,
+        #pinecone.create_index(index_name, dimension=1536,
                               metric="cosine", pods=1, pod_type="p1.x1")
         
     text_splitter = TokenTextSplitter(chunk_size=1000, chunk_overlap=0)
@@ -66,7 +66,7 @@ def operation():
         pdf_name = pdf['Name']
         pdf_file_path = os.path.join(sp_file_path, pdf_name).replace('\\','/')
         #print (pdf_file_path)
-        ##st.write(pdf_file_path)
+        st.write(pdf_file_path)
         pdf_stream = get_pdf_stream(pdf_name)
     
         # Extract text from PDF using PyPDF2
