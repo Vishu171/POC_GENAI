994ddaec100f749a1a245d9857299f0d76bda0ec
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-02 12:23:56 +0530

    Update buffett_app.py

--- buffett_app.py
+++ buffett_app.py
@@ -45,8 +45,8 @@ if authenticate_user():
     with st.sidebar:
       image = Image.open("assets/JadeLogo.png")
       image = st.image('assets/JadeLogo.png',width=250)
-      image = Image.open("assets/Newsidelogo1.png")
-      image = st.image('assets/Newsidelogo1.png',width=265)
+      image = Image.open("assets/Knowledgelogo.png")
+      image = st.image('assets/Knowledgelogo.png',width=265)
    
     image = Image.open("assets/KB_Top1.jpg")
     new_image = image.resize((830, 100))
