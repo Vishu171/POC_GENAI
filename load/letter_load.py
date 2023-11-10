abdf3133d7e372c2e595b266fdff764a2ebdb9b5
Vishu171 <100827321+Vishu171@users.noreply.github.com>
2023-11-10 22:55:37 +0530

    Update letter_load.py

--- load/letter_load.py
+++ load/letter_load.py
@@ -66,7 +66,7 @@ def operation():
         pdf_name = pdf['Name']
         pdf_file_path = os.path.join(sp_file_path, pdf_name).replace('\\','/')
         #print (pdf_file_path)
-        st.write(pdf_file_path)
+        ##st.write(pdf_file_path)
         pdf_stream = get_pdf_stream(pdf_name)
     
         # Extract text from PDF using PyPDF2
